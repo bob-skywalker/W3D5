595329e8ea6c518e68a7dd10700449d9218b19bb
q463250938@gmail.com <q463250938@gmail.com>
2022-07-01 14:00:01 -0700

    first 2 firnished

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -18,16 +18,37 @@ class PolyTreeNode
     end
 
     def parent=(node)
+        if @parent != nil
+            self.parent.children.delete(self)
+        end
+        self.parent
         @parent = node
+        return nil if node == nil
+        # return nil if self == nil
 
-        return nil if @parent.nil? 
         
-        unless @parent.children.include?(self)
-            @parent.children << self
-        end 
+
+        # @parent = node
+
+        unless node.children.include?(self)
+            node.children << self 
+
+        end
+        # # self.parent.children - [self]
+        # if @parent != nil
+        #     self.parent.children - [self]
+        # end
+        # # return nil if @parent.nil? 
+        # @parent = node
+        
+        # unless @parent.children.include?(self)
+        #     @parent.children << self
+        # end 
 
     
     end 
+
+    
 
 
 end 
