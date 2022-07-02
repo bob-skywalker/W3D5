3cbc125027d6c454ccdf8632d28ab53ea891902c
q463250938@gmail.com <q463250938@gmail.com>
2022-07-01 17:17:11 -0700

    finished

--- knights_travails_project/knights_travails.rb
+++ knights_travails_project/knights_travails.rb
@@ -1,15 +1,39 @@
 require_relative './poly.rb'
 
 class KnightPathFinder
-    
+    attr_reader :root_node
+
+self.valid_moves(pos)
+    x,y = pos
+    DICTIONARY = [[x+2,y+1],[x+1,y+2],[x-1,y-2],[x-2,y-1],[x-1,y+2],[x+2,y-1],[x+1,y-2],[x-2,y+1]]
+end
+
+self.is_valid?(pos)
+
+x,y = pos
+return false if x < 0 || y < 0 || x > 7 || y > 7
+
+end
+
+
+end
+
     def initialize(pos)
-        @root_node = pos
+        @root_node = PolyTreeNode.new(pos)
+        @considered_positions = [pos]
+        
     end
 
     def build_move_tree()
     end
 
     def find_path(pos)
+    end
+
+    def new_move_positions(pos)
+
+        if KnightPathFinder.valid_moves + pos
+        
     end
 
     
